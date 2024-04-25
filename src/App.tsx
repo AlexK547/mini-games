0de12587291052069223adfbf7419d8c07d8ac93
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-25 22:07:40 +0400

    add settings for deploy gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Game2048 from './components/game2048/Game2048'
 import GameMemoryGame from './components/memory-game/GameMemoryGame'
@@ -7,15 +8,14 @@ import GameSudoku from './components/sudoku/GameSudoku'
 function App() {
 
   return (
-    <>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className='wrapper'>
         <h1>Mini games</h1>
         <Game2048 />
         <GameMemoryGame />
         <GameSudoku />
-
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
